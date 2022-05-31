b7524faec59e8d564f2b57c5deef7e22279c029b
dingtong8 <2676799689@qq.com>
2022-05-31 17:52:25 +0800

    修改MySQL日志过滤命令

--- mysql-check/files/check_mysql.sh
+++ mysql-check/files/check_mysql.sh
@@ -7,7 +7,7 @@
 function check_log_mysql() {
   # 9463 127.0.0.1 6 192.168.31.203
   #log_failed_ssh=$(sudo cat `sudo find /var/log/ -name 'secure*' -newermt 2022-04-01 ! -newermt $(date -d next-day +%Y-%m-%d)` | awk '/Failed password/{print $(NF-3)}' | grep "^[0-9]" | sort | uniq -c | sort -nr | awk '{print "'\''"$2"'\''"":""'\''"$1"'\''"","}')
-  log_failed_mysql=$(sudo cat `sudo find /var/log/ -type f -name 'mysql*' -newermt $(date +%Y-%m-01) ! -newermt $(date -d next-day +%Y-%m-%d)` | awk '/Access denied/{print $8}' | grep '@' | tr -d "'" | sort | uniq -c | sort -nr | awk '{print "'\''"$2"'\''"":""'\''"$1"'\''"","}')
+  log_failed_mysql=$(sudo cat `sudo find /var/log/ -type f -name 'mysql*' -newermt $(date +%Y-%m-01) ! -newermt $(date -d next-day +%Y-%m-%d)` | grep $(date +%Y-%m-) | grep '@' | awk -F'@| ' '/Access denied/{print $9}' | tr -d "'" | sort | uniq -c | sort -nr | awk '{print "'\''"$2"'\''"":""'\''"$1"'\''"","}')
   mysql_facts=$(
   cat <<EOF
   {
